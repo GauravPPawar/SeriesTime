2b3411f7b05200410d42341541965450e1a8f848
GauravPPawar <gauravppawar8@gmail.com>
2022-04-08 02:30:12 +0530

    done with function module structures

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
+from cProfile import label
 from flask import Flask, render_template
+import json
+import http.client
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    data = [
-        ("1", 9.5),
-        ("2", 9.6),
-        ("3", 9.1),
-        ("4", 8.6),
-        ("5", 8.5),
-        ("6", 8.9),
-        ("7", 9.5),
-        ("8", 9.6),
-        ("9", 9.5),
-        ("10", 9.7),
-    ]
-
+    seriesKey = "tt2364582"
+    season = "1"
+    seriesName, data = getIMDBData(seriesKey, season)
     # labels = [row[0] for row in data]
     # values = [row[1] for row in data]
     labels = []
@@ -24,8 +17,27 @@ def home():
     for row in data:
         labels.append(row[0])
         values.append(row[1])
+    return render_template("darkGraph.html", seriesName = seriesName, labels = labels, values = values)
 
-    return render_template("darkGraph.html", labels = labels, values = values)
+def getIMDBData(seriesKey, season):
+    labels = []
+    values = []
+
+    conn = http.client.HTTPSConnection("imdb-api.com", 443)
+    payload = ''
+    headers = {}
+    conn.request("GET", "https://imdb-api.com/API/SeasonEpisodes/k_37wugmrf/" + seriesKey +"/"+season, payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    # print(data.decode("utf-8"))
+    mvDict = json.loads(data.decode("utf-8"))
+
+    title = mvDict['title']
+    dt = []
+    for i in mvDict['episodes']:
+        dt.append((i['episodeNumber'],float(i['imDbRating'])))
+
+    return title, dt
 
 if __name__ == "__main__":
     app.run( host = "192.168.43.166",debug = True)
