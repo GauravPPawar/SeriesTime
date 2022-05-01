f2d38e5eeb075759d16e0099a2713357895eba12
GauravPPawar <gauravppawar8@gmail.com>
2022-05-01 13:48:15 +0530

    mid sem code

--- app.py
+++ app.py
@@ -7,22 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    seriesKey = "tt2364582"
-    season = "1"
-    seriesName, data = getIMDBData(seriesKey, season)
-    # labels = [row[0] for row in data]
-    # values = [row[1] for row in data]
-    labels = []
-    values = []
-    for row in data:
-        labels.append(row[0])
-        values.append(row[1])
+    seriesKey = "tt0944947"
+    season = "8"
+    seriesName, labels, values = getIMDBData(seriesKey, season)
+    
     return render_template("darkGraph.html", seriesName = seriesName, labels = labels, values = values)
 
 def getIMDBData(seriesKey, season):
     labels = []
     values = []
-
+    
     conn = http.client.HTTPSConnection("imdb-api.com", 443)
     payload = ''
     headers = {}
@@ -33,11 +27,12 @@ def getIMDBData(seriesKey, season):
     mvDict = json.loads(data.decode("utf-8"))
 
     title = mvDict['title']
-    dt = []
-    for i in mvDict['episodes']:
-        dt.append((i['episodeNumber'],float(i['imDbRating'])))
 
-    return title, dt
+    for i in mvDict['episodes']:
+        labels.append((i['episodeNumber']))
+        values.append(float(i['imDbRating']))
+
+    return title, labels, values
 
 if __name__ == "__main__":
     app.run( host = "192.168.43.166",debug = True)
